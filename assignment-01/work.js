b6a6c25c437bda2f3c26324902788298c1d027c9
Humna-ghouri <monababu001@gmail.com>
2024-10-20 09:48:55 -0700

    done 1st assignment

--- assignment-01/work.js
+++ assignment-01/work.js
@@ -1,35 +1,33 @@
+// 1.Write a script to greet your website visitor using JS alert
+// box.
+// alert("Welcome to our website!");
+
+// 2.Write a script to display following message on your web page:
+// document.getElementById("message").textContent = "Welcome to our website! Enjoy your stay.";
+
+// 3.Write a script to display following message on your webpage: (Hint : Use line break)
+// document.getElementById("message").innerHTML = "Welcome to our website!<br>Enjoy your stay.<br>Have a great day!";
+
+// 4.Write a script to display following messages in sequence:
+// alert("Message 1: Welcome to our website!");
+// alert("Message 2: Enjoy exploring our content.");
+// alert("Message 3: Have a great day!");
 
 
-
-// alert("Error! Plaese enter a valid password.");
-// alert("Welcome to JS Land... \n  Happy Coding!")
-// window.onload = function() {
-//     alert('Welcome to JS Land....');
-//     alert('Happy Coding! \n ');
+// 5.Generate the following message through browser’s developer console:
+// console.log("Welcome to our website!");
+// console.log("Enjoy exploring our content.");
+// console.log("Have a great day!");
 
 
-alert('This is the third alert message. \n ');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 6. Make use of alerts in your new/existing HTML & CSS project.
 
 
 
 
-//     alert('This is the third alert message.');
-//     alert('This is the fourth alert message.');
-//     alert('This is the fifth alert message.');
-// };
+// 7.
+// Practice placement of <script></script> element in following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
